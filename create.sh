f7ca386604d41f469aee7e9e8263c91d73b6fe35
Piot Gacek <pgacek@linux.pl>
2018-02-03 00:21:37 +0100

    single for multiple users

--- create.sh
+++ create.sh
@@ -16,37 +16,65 @@ fi
 ### get user names - space as separator
 
 printf "Enter username(s) here: "
-IFS=' ' read -a user_names
+IFS=' ' read user_names
 
 ### exit if no users provided
-### or change password
 
-total_users="${#user_names[@]}"
-
-if [[ -z "${#user_names[@]}" ]]; then
+if [[ -z "$user_names"  ]]; then
 	echo -e ""${RED}"Nothing to do here"${NC}""
-
 else
-	for (( user_no = 0; user_no <= $(( $total_users -1 )); user_no++ )) do
-		echo "$user_no"
-		for i in "${user_names[$user_no]}"; do
-			echo "$user_no"
-			# check if user exist if yes - change passwd
-			getent passwd "${user_names[$i]}" > /dev/null 2>&1
-			if [[ "$?" -eq "0" ]]; then
-				echo "${user_names[$i]}"
+	for i in $user_names; do
+		getent passwd $i > /dev/null 2>&1
+		if [[ "$?" -eq "0" ]]; then
 
-			# if no create and set passwd
-			else
-				useradd "${user_names[$i]}"
-				echo -e ""${RED}"User "${GREEN}""${user_names[$i]}""${RED}" created "${NC}""
-			fi
-		done
-
-		#userdel -r "${user_names[$i]}"
+		# if no create and set passwd
+		else
+			useradd $i
+			echo -e ""${RED}"User "${GREEN}" $i "${RED}" created "${NC}""
+		fi
+		#userdel -r "${user_names[$user_no]}"
 	done
 fi
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# total_users="${#user_names[@]}"
+
+# if [[ -z "${#user_names[@]}" ]]; then
+# 	echo -e ""${RED}"Nothing to do here"${NC}""
+
+# else
+# 	for (( user_no = 0; user_no <= $(( $total_users -1 )); user_no++ )); do
+# 		echo "$user_no"
+
+# 		# check if user exist if yes - change passwd
+# 		getent passwd "${user_names[$user_no]}" > /dev/null 2>&1
+# 		if [[ "$?" -eq "0" ]]; then
+# 			echo "${user_names[$user_no]}"
+
+# 		# if no create and set passwd
+# 		else
+# 			useradd "${user_names[$user_no]}"
+# 			echo -e ""${RED}"User "${GREEN}""${user_names[$user_no]}""${RED}" created "${NC}""
+# 		fi
+# 		userdel -r "${user_names[$user_no]}"
+# 	done
+
 	
-
-
+# fi
